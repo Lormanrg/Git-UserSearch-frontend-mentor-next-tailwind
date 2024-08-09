57a26a228b1a785dff2484f3d25735bf3df9f67f
Lorman Romero <lormanrg@gmail.com>
2024-08-09 01:20:12 -0400

    :construction: Applying fetch to bring all users information

--- src/app/components/FormSearchUser.tsx
+++ src/app/components/FormSearchUser.tsx
@@ -1,13 +1,23 @@
+
+"use client"
 import React from 'react'
 import SearchIcon from "./icons/SearchIcon"
 
+const handleSubmit = (e:React.FormEvent<HTMLFormElement>)=>{
+  e.preventDefault()
+  
+  const username = e.currentTarget.username.value
+if(!username) return
+}
 const FormSearchUser = () => {
   return (
-    <form className=" gap-2 flex flex-wrap bg-blue-900 p-4 rounded-xl items-center mb-6">
+    <form 
+    onSubmit={handleSubmit}
+     className=" gap-2 flex flex-wrap bg-blue-900 p-4 rounded-xl items-center mb-6">
     <span className="min-w-[20px]">
       <SearchIcon className="fill-sky-400" />
     </span>
-    <input type="text" placeholder="Search Github Username" className=" flex-1 h-14 p-2 rounded-lg bg-transparent text-white placeholder:text-white focus:outline-none focus:ring-2 focus: ring-blue-500 " />
+    <input name='username' type="text" placeholder="Search Github Username" className=" flex-1 h-14 p-2 rounded-lg bg-transparent text-white placeholder:text-white focus:outline-none focus:ring-2 focus: ring-blue-500 " />
     <button className="bg-sky-400 rounded-lg py-4 px-4 text-white font-bold">Search</button>
   </form>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,20 @@ import React from "react";
 import FormSearchUser from "./components/FormSearchUser";
 import UserCardInfo from "./components/UserCardInfo";
 
-const Home = () => {
+const  getUser =  async (username: string)=>{
+
+  const res = await fetch(`https://api.github.com/users/${username}`)
+  const data = await res.json()
+
+  return data
+} 
+
+const Home = async () => {
+
+  const data = await getUser("lormanrg")
+
+
+  
   return (
     <>
     <FormSearchUser/>
